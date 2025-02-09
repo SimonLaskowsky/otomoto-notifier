63f883d3df46d6900e2701724e260622518003a2
Szymon Laskowski <simonlaskowsky@gmail.com>
2025-02-09 22:57:37 +0100

    Added config file

--- filters.py
+++ filters.py
@@ -1,3 +1,15 @@
-def is_good_deal(car):
-    """Sprawdza, czy oferta jest dobra (przykładowe warunki)"""
-    return car["price"] < 50000 and car["year"] > 2015
+def is_good_deal(car, config):
+    """Sprawdza ofertę względem konfiguracji"""
+    basic_checks = [
+        car["price"] <= config["max_price"],
+        car["year"] >= config["min_year"]
+    ]
+    
+    if config["allowed_brands"]:
+        brand = car["title"].split()[0]
+        basic_checks.append(brand in config["allowed_brands"])
+    
+    if config["max_mileage"] is not None:
+        basic_checks.append(car.get("mileage", 0) <= config["max_mileage"])
+    
+    return all(basic_checks)
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
+import json
 from filters import is_good_deal
 from notifier import send_sms
 
-# Symulacja pobranych ofert (docelowo API Otomoto)
+def load_config():
+    """Wczytuje konfigurację z pliku JSON z domyślnymi wartościami"""
+    default_config = {
+        "max_price": 50000,
+        "min_year": 2015,
+        "allowed_brands": [],
+        "max_mileage": None
+    }
+    
+    try:
+        with open('config.json') as f:
+            config = json.load(f)
+            return {**default_config, **config}
+    except FileNotFoundError:
+        return default_config
+
+# Wczytaj konfigurację
+config = load_config()
+
+# Symulacja pobranych ofert
 offers = [
-    {"title": "Audi A4 2018", "price": 45000, "year": 2018, "url": "https://otomoto.pl/audi1"},
-    {"title": "BMW 3 2016", "price": 52000, "year": 2016, "url": "https://otomoto.pl/bmw2"},
-    {"title": "Skoda Octavia 2019", "price": 49000, "year": 2019, "url": "https://otomoto.pl/skoda3"},
+    {"title": "Audi A4 2018", "price": 45000, "year": 2018, "url": "https://otomoto.pl/audi1", "mileage": 120000},
+    {"title": "BMW 3 2016", "price": 52000, "year": 2016, "url": "https://otomoto.pl/bmw2", "mileage": 180000},
+    {"title": "Skoda Octavia 2019", "price": 49000, "year": 2019, "url": "https://otomoto.pl/skoda3", "mileage": 90000},
 ]
 
-# Przeszukujemy oferty
 for car in offers:
-    if is_good_deal(car):
+    if is_good_deal(car, config):
         message = f"🚗 {car['title']} za {car['price']} zł! 🔥 {car['url']}"
         send_sms(message)
